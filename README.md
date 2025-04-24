da7fd84c778d57d3e4bbcb77ae7d36e36d6e98f9
MateusGit123 <mateus.veloso@fatec.sp.gov.br>
2025-04-24 17:36:20 -0300

    docs(p1): inclui nomes e ras

---
+++ README.md
@@ -0,0 +1,9 @@
+# Consulta de Endereço por CEP - P1
+
+## Integrantes
+
+Nome:
+RA:
+
+Nome: Mateus Isidorio Veloso
+RA: 2040482212003
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
